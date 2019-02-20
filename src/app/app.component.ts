9d85bab12b5b84b2be388934ab0a8a390dd30132
Chris Samuel <ksamuel.chris@gmail.com>
2019-02-20 14:32:40 -0500

    generate a random fixed number of columns and rows

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit   } from '@angular/core';
+// import { ConsoleReporter } from 'jasmine';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'game-of-life';
 
 
+
+
+ngOnInit() {
+ const grid =  this.createTwoDimensionalArray(10, 10);
+ console.log(grid);
+
+}
+
   createTwoDimensionalArray(columns, rows) {
-    let arr = new Array(columns);
-    for (let i = 0; i <arr.length; i++) {
+    const arr = new Array(columns);
+    for (let i = 0; i < arr.length; i++) {
       arr[i] = new Array(rows);
     }
   }
+
+
+
 }
